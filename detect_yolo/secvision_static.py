a63082fe10b61f324b51091222474455f30d4201
Psynosaur <psynosaur@gmail.com>
2021-12-19 12:14:20 +0200

    Some more commentary

--- detect_yolo/secvision_static.py
+++ detect_yolo/secvision_static.py
@@ -18,77 +18,79 @@ channel_names = {
 
 def initworkers(obj) -> None:
     e = threading.Event()
-    heartbeatworker = threading.Thread(name='heartbeat-daemon', target=recorder_thread,
-                                        args=(e, obj, 5))
-    heartbeatworker.setDaemon(True)
-    heartbeatworker.start()
+    channel_event_worker = threading.Thread(name='channel-event-daemon', target=channel_event_work,
+                                            args=(e, obj, 5))
+    channel_event_worker.setDaemon(True)
+    channel_event_worker.start()
 
     webserver = threading.Thread(target=run_server, args=(aiohttp_server(obj),))
     webserver.start()
 
 
-
-def recorder_thread(event: threading.Event, obj, time: float) -> None:
+def channel_event_work(event: threading.Event, obj, time: float) -> None:
     while not event.isSet():
-        thread_zone_garbage_collector = []
-        thread_channel_done_event = []
+        zone_done_event_handler = []
+        channel_done_event_handler = []
 
         event_is_set = event.wait(time)
         if event_is_set:
             logging.debug('processing event')
         else:
-            obj.cnt += 1
             # Give us stats every minute or so . . .
+            obj.cnt += 1
             if obj.cnt == 12:
                 obj.cnt = 0
                 ao_temp, cpu_temp, gpu_temp, pll_temp, rpm, thermal = jetson_metrics()
                 net_speed = sum(obj.network_speed) / len(obj.network_speed)
                 log_metrics(ao_temp, cpu_temp, gpu_temp, pll_temp, rpm, thermal, net_speed)
-                # data = obj.dbObj[-1]
-                # person = f"{data['persons']} persons" if int(data['persons']) > 1 else f"{data['persons']} person"
-                # display_date = data['time'][11:22]
 
-                # logging.info(
-                #     f" Last Detection : {display_date}: {channel_names[data['channel']]} -> {person} found")
+            # Do some work if there is a channel event
             if len(obj.sv_channel_event) > 0:
                 for channel in obj.sv_channel_event:
+                    # Checks channel event timestamp
                     if float(obj.sv_channel_event[channel]) > 0:
                         elapsed = datetime.datetime.now() - datetime.datetime.fromtimestamp(
                             obj.sv_channel_event[channel])
                         logging.warning(
                             f" {channel_names[channel]} person found {elapsed.total_seconds()}s ago")
 
+                        # Checks if the channel event is older than 15s and then stops the zone recording.
+                        # TODO: Zoneless operation
+                        #  These zones would be redundant if recordings where triggered on a per channel basis
+                        #  non electrically...
                         if elapsed > datetime.timedelta(seconds=15):
                             zone = obj.determine_zone(channel)
                             if zone == 1:
                                 if len(obj.zone1) == 1:
-                                    thread_zone_garbage_collector.append(channel)
+                                    zone_done_event_handler.append(channel)
                                 else:
-                                    thread_channel_done_event.append(channel)
+                                    channel_done_event_handler.append(channel)
                                 obj.zone1.pop(channel, None)
                             elif zone == 2:
                                 if len(obj.zone2) == 1:
-                                    thread_zone_garbage_collector.append(channel)
+                                    zone_done_event_handler.append(channel)
                                 else:
-                                    thread_channel_done_event.append(channel)
+                                    channel_done_event_handler.append(channel)
                                 obj.zone2.pop(channel, None)
                             elif zone == 3:
                                 if len(obj.zone3) == 1:
-                                    thread_zone_garbage_collector.append(channel)
+                                    zone_done_event_handler.append(channel)
                                 else:
-                                    thread_channel_done_event.append(channel)
+                                    channel_done_event_handler.append(channel)
                                 obj.zone3.pop(channel, None)
                             else:
                                 if len(obj.zone4) == 1:
-                                    thread_zone_garbage_collector.append(channel)
+                                    zone_done_event_handler.append(channel)
                                 else:
-                                    thread_channel_done_event.append(channel)
+                                    channel_done_event_handler.append(channel)
                                 obj.zone4.pop(channel, None)
 
-                for channel in thread_channel_done_event:
+                # Does work on the channel done event
+                for channel in channel_done_event_handler:
                     obj.sv_channel_event.pop(channel, None)
 
-                for channel in thread_zone_garbage_collector:
+                # Does work on the zone done event
+                for channel in zone_done_event_handler:
                     obj.sv_channel_event.pop(channel, None)
                     obj.sv_garbage_collector.append(channel)
 
@@ -101,6 +103,7 @@ def log_metrics(ao_temp, cpu_temp, gpu_temp, pll_temp, rpm, thermal, net):
         f" AO {int(ao_temp) / 1000:.2f}°C / THERM {int(thermal) / 1000:.2f}°C / FAN {round(rpm, 0)}RPM ")
     logging.info(
         f" NETWORK {net:.2f} FPS ")
+
 
 def jetson_metrics():
     fan_rpm = 2000
